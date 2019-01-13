466c94bdca01e0b79b2821f9d6808ed19e2fc05b
Yasuaki Yano <he.yasu00@gmail.com>
2019-01-13 19:51:47 +0900

    オブジェクト調整。

--- assets/js/sample_2.js
+++ assets/js/sample_2.js
@@ -1,12 +1,12 @@
 var boxEl = document.querySelector('a-box');
 // カーソルがぶつかったら拡大
 boxEl.addEventListener('mouseenter', function() {
-    boxEl.setAttribute('width', 10);
+    boxEl.setAttribute('width', 5);
     console.log("mouseenter");
 });
 
 // カーソルが離れたら元にもどす
 boxEl.addEventListener('mouseleave', function() {
-    boxEl.setAttribute('width', 3);
+    boxEl.setAttribute('width', 2);
     console.log("mouseleave");
 });
