d519bfe527a9aeea7f93edce4430bad627597444
Yasuaki Yano <he.yasu00@gmail.com>
2019-01-15 17:39:18 +0900

    modosu

--- assets/js/component/sample_2_component.js
+++
@@ -1,9 +0,0 @@
-AFRAME.registerComponent('log', {
-    scheme: {
-        message: {type: 'string', default: 'Hello, World!'}
-    },
-
-    init: function() {
-        console.log(this.data.message);
-    }
-)};
