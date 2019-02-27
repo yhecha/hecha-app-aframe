1d73a60a818e429d0bef92ea63ea07289c176f0a
Yasuaki Yano <he.yasu00@gmail.com>
2019-02-27 23:18:59 +0900

    インデックス名変更

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <body>
         <ul>
             <li><a href="sample.html">公式のサンプルをコピペ</a></li>
-            <li><a href="sample_2.html">注視カーソル</a></li>
+            <li><a href="sample_2.html">注視カーソルと画像の切り替え</a></li>
             <li><a href="sample_3.html">物理演算(サイコロが落ちてくるやつコピペ)</a></li>
             <li><a href="sample_4.html">アニメーション(地球)</a></li>
             <li><a href="sample_5.html">AR(地球)</a></li>
